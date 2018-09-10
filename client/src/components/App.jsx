2bcc09ebd2719ef5188a9ca99aca699da92e70ce
Samuel Shih <samuels.shih@gmail.com>
2018-09-09 23:50:20 -0700

    Started on Contact Module

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactInfo from './ContactInfo.jsx';
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <h1 className="logo">Zagat</h1>
+      <div className="placeholder" />
+      <ContactInfo />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/ContactInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Modal from './Modal.jsx';
+
+const ContactInfo = () => {
+  return (
+    <div className="contact-container">
+      <h4>Restaurant Times</h4>
+      <h4>Address</h4>
+      <h4>Phone Number</h4>
+      <h4>Website</h4>
+      <h4>Get Directions</h4>
+      <h4>Map</h4>
+      <Modal />
+    </div>
+  );
+};
+
+export default ContactInfo;
