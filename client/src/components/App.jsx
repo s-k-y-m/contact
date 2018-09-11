73685dcaf4908aa9b336edffbe8d4df995d26924
Samuel Shih <samuels.shih@gmail.com>
2018-09-10 21:15:02 -0700

    Client, server, and database talking

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import ContactInfo from './ContactInfo.jsx';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div className="main-container">
-      <h1 className="logo">Zagat</h1>
-      <div className="placeholder" />
-      <ContactInfo />
-    </div>
-  );
-};
+var fakeData = require('../../../database/fakeData/fakeRestaurants.js');
+var restaurant = fakeData.oneRestaurant;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      restaurant: restaurant
+    };
+  }
+  componentDidMount() {
+    var restaurant;
+    axios
+      .get('http://localhost:3005/restaurants/name')
+      .then(data => {
+        restaurant = data.data;
+        console.log('Restaurant', restaurant);
+        this.setState({
+          restaurant: restaurant
+        });
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
+  render() {
+    const { restaurant } = this.state;
+    return (
+      <div className="main-container">
+        <h1 className="logo">Zagat</h1>
+        <div className="placeholder" />
+        <ContactInfo restaurant={restaurant} />
+      </div>
+    );
+  }
+}
 
 export default App;
