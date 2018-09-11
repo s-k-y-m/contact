bbb283dc542094ca418681c5fb07facc970ddc83
Samuel Shih <samuels.shih@gmail.com>
2018-09-11 16:13:24 -0700

    Added animation to arrow. Need to refactor

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo.jsx';
 import axios from 'axios';
+import { VelocityComponent } from 'velocity-react';
 
 var fakeData = require('../../../database/fakeData/fakeRestaurants.js');
 var restaurant = fakeData.oneRestaurant;
@@ -8,9 +9,11 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      restaurant: restaurant
+      restaurant: restaurant,
+      expanded: true
     };
   }
+
   componentDidMount() {
     var restaurant;
     axios
@@ -25,11 +28,26 @@ class App extends React.Component {
         console.log('Error', err);
       });
   }
+
+  handleToggle() {
+    console.log('H1 toggled');
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
+
   render() {
     const { restaurant } = this.state;
     return (
       <div className="main-container">
-        <h1 className="logo">Zagat</h1>
+        <VelocityComponent
+          animation={{ rotateZ: this.state.expanded ? 0 : -180 }}
+          duration={500}
+        >
+          <h1 className="logo" onClick={this.handleToggle.bind(this)}>
+            Zagat
+          </h1>
+        </VelocityComponent>
         <div className="placeholder" />
         <ContactInfo restaurant={restaurant} />
       </div>
--- client/src/components/OpenNow.jsx
+++ client/src/components/OpenNow.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FaChevronDown } from 'react-icons/fa';
+import { VelocityComponent } from 'velocity-react';
 
 class OpenNow extends React.Component {
   constructor() {
     super();
     this.state = {
-      toggled: false
+      toggled: false,
+      expanded: false
     };
   }
 
   handleToggle() {
     this.setState({
-      toggled: !this.state.toggled
+      toggled: !this.state.toggled,
+      expanded: !this.state.toggled
     });
   }
 
@@ -28,66 +31,73 @@ class OpenNow extends React.Component {
 
     const currentDay = this.props.currentDay;
 
-    if (this.state.toggled === true) {
-      return (
-        <div className="wholeSchedule">
-          <div
-            className="oneDaySchedule"
-            onClick={this.handleToggle.bind(this)}
-          >
-            <div>
-              <span className="openNowOneLine">Open Now</span>{' '}
-              {this.props.times[currentDay.toLowerCase()]}
-              <div>
-                <FaChevronDown />
-              </div>
-            </div>
-          </div>
-          <div className="schedule" onClick={this.handleToggle.bind(this)}>
-            <div id="Monday">
-              Monday.........................
-              {monday}
-            </div>
-            <div id="Tuesday">
-              Tuesday.........................
-              {tuesday}
-            </div>
-            <div id="Wednesday">
-              Wednesday.........................
-              {wednesday}
-            </div>
-            <div id="Thursday">
-              Thursday.........................
-              {thursday}
-            </div>
-            <div id="Friday">
-              Friday.........................
-              {friday}
-            </div>
-            <div id="Saturday">
-              Saturday.........................
-              {saturday}
-            </div>
-            <div id="Sunday">
-              Sunday.........................
-              {sunday}
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
+    // if (this.state.toggled === true) {
+    return (
+      <div className="wholeSchedule">
         <div className="oneDaySchedule" onClick={this.handleToggle.bind(this)}>
           <div>
             <span className="openNowOneLine">Open Now</span>{' '}
             {this.props.times[currentDay.toLowerCase()]}
-            <div>
-              <FaChevronDown />
+            <div className="chevron">
+              <VelocityComponent
+                animation={{ rotateZ: this.state.expanded ? 180 : 0 }}
+                duration={500}
+              >
+                <FaChevronDown />
+              </VelocityComponent>
             </div>
           </div>
         </div>
-      );
-    }
+        <div className="schedule" onClick={this.handleToggle.bind(this)}>
+          <div id="Monday">
+            Monday.........................
+            {monday}
+          </div>
+          <div id="Tuesday">
+            Tuesday.........................
+            {tuesday}
+          </div>
+          <div id="Wednesday">
+            Wednesday.........................
+            {wednesday}
+          </div>
+          <div id="Thursday">
+            Thursday.........................
+            {thursday}
+          </div>
+          <div id="Friday">
+            Friday.........................
+            {friday}
+          </div>
+          <div id="Saturday">
+            Saturday.........................
+            {saturday}
+          </div>
+          <div id="Sunday">
+            Sunday.........................
+            {sunday}
+          </div>
+        </div>
+      </div>
+    );
+    // } else {
+    //   return (
+    //     <div className="oneDaySchedule" onClick={this.handleToggle.bind(this)}>
+    //       <div>
+    //         <span className="openNowOneLine">Open Now</span>{' '}
+    //         {this.props.times[currentDay.toLowerCase()]}
+    //         <div className="chevronDown">
+    //           <VelocityComponent
+    //             animation={{ rotateZ: this.state.expanded ? 0 : -180 }}
+    //             duration={500}
+    //           >
+    //             <FaChevronDown />
+    //           </VelocityComponent>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   );
+    // }
   }
 }
 
