9391d8a1f5aa966601712ca0ff06cad7e9d8deaa
Samuel Shih <samuels.shih@gmail.com>
2018-09-10 17:37:56 -0700

    Revert "Added padding and connected app to database"
    This reverts commit bc3f4a31d6a8014bd3dbfcb274603672a600f203.

--- client/src/components/ContactInfo.jsx
+++ client/src/components/ContactInfo.jsx
@@ -6,42 +6,32 @@ import PhoneIcon from './icons/PhoneIcon.jsx';
 import WebsiteIcon from './icons/WebsiteIcon.jsx';
 import DirectionsIcon from './icons/DirectionsIcon.jsx';
 
-const ContactInfo = props => {
-  const {
-    openNow,
-    name,
-    address,
-    phoneNumber,
-    websiteURL,
-    getDirectionsURL
-  } = props.restaurant;
-
-  console.log(openNow, name, address);
-
+const ContactInfo = () => {
   return (
     <div className="contact-container">
       <div className="restaurant-times">
         <ClockIcon />
-        <p className="contact-text">Restaurant Times</p>
+        <p>Restaurant Times</p>
       </div>
       <div className="restaurant-address">
         <MapIcon />
-        <p className="contact-text">{address}</p>
+        <p>Address</p>
       </div>
       <div className="restaurant-phoneNumber">
         <PhoneIcon />
-        <p className="contact-text">{phoneNumber}</p>
+        <p>Phone Number</p>
       </div>
       <div className="restaurant-website">
         <WebsiteIcon />
-        <p className="contact-text">{websiteURL}</p>
+        <p>Website</p>
       </div>
       <div className="restaurant-directions">
         <DirectionsIcon />
-        <p className="contact-text">Get Directions</p>
+        <p>Get Directions</p>
       </div>
       <div className="restaurant-map">
         <Modal />
+        <p>Map</p>
       </div>
     </div>
   );
