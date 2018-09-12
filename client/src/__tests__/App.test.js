895afe933953b7250fedb5666f9eba6cfe10a4d4
Samuel Shih <samuels.shih@gmail.com>
2018-09-12 15:50:01 -0700

    Added test coverage for puppeteer, jest, and enzyme. Embedded Google Maps in the contact section and the modal

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,27 +1,48 @@
-// import React from 'react';
-// import { shallow, mount, render } from 'enzyme';
-// import App from '../components/App.jsx';
-// import axios from 'axios';
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import App from '../components/App';
+import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallowToJson } from 'enzyme-to-json';
 
-// describe('Component rendering', function() {
-//   it('should render without throwing an error', function() {
-//     expect(shallow(<App />).contains('Zagat')).toBe(true);
-//   });
+configure({ adapter: new Adapter() });
 
-//   it('should be selectable by class "foo"', function() {
-//     expect(shallow(<App />).is('.main-container')).toBe(true);
-//   });
+var axios = require('axios');
 
-//   it('should mount in a full DOM', function() {
-//     expect(mount(<App />).find('.main-container').length).toBe(1);
-//   });
-// });
+const pageUrl = 'http://localhost:3005/restaurants/name';
 
-// describe('api call receiving data', () => {
-//   test('the data is peanut butter', () => {
-//     expect.assertions(1);
-//     return fetchData().then(data => {
-//       expect(data).toBe('peanut butter');
-//     });
-//   });
-// });
+describe('Snapshot testing', () => {
+  it('should render correctly', () => {
+    const output = shallow(<App />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
+
+describe('Component rendering', function() {
+  it('should render without throwing an error', () => {
+    expect(shallow(<App />).contains('Zagat')).toBe(true);
+  });
+
+  it('should be selectable by class "main container"', () => {
+    expect(shallow(<App />).is('.main-container')).toBe(true);
+  });
+
+  it('should mount in a full DOM', () => {
+    expect(mount(<App />).find('.main-container').length).toBe(1);
+  });
+});
+
+describe('api call receiving data', () => {
+  test('the data is a restaurant', () => {
+    axios
+      .get(pageUrl)
+      .then(data => {
+        restaurant = data.data;
+        expect(restaurant.name).toBe('Hilll Group');
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  });
+});
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,18 +5,26 @@ const ModalModalExample = () => (
   <Modal trigger={<Button>Expand Map</Button>}>
     <Modal.Header>Map Image</Modal.Header>
     <Modal.Content image>
-      <Image
-        wrapped
-        size="medium"
-        src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
-      />
       <Modal.Description>
-        <Header>Testing!</Header>
-        <p>
-          We've found the following gravatar image associated with your e-mail
-          address.
-        </p>
-        <p>Is it okay to use this photo?</p>
+        <Header>Restaurant Name</Header>
+        <div>
+          <iframe
+            width="100%"
+            height="600"
+            src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          >
+            <a href="https://www.maps.ie/create-google-map/">Get Directions</a>
+          </iframe>
+        </div>
+        <div class="ui button">
+          <a href="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;">
+            Neutral
+          </a>
+        </div>
       </Modal.Description>
     </Modal.Content>
   </Modal>
