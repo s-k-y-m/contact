0f5a6013941604c672d422d8fe826221b2739266
Samuel Shih <samuels.shih@gmail.com>
2018-09-11 17:08:33 -0700

    Cleaned up code, got arrow to work

--- client/src/components/OpenNow.jsx
+++ client/src/components/OpenNow.jsx
@@ -18,8 +18,52 @@ class OpenNow extends React.Component {
     });
   }
 
+  showSchedule() {
+    let {
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+      saturday,
+      sunday
+    } = this.props.times;
+    return (
+      <div className="schedule" onClick={this.handleToggle.bind(this)}>
+        <div id="Monday">
+          Monday.........................
+          {monday}
+        </div>
+        <div id="Tuesday">
+          Tuesday.........................
+          {tuesday}
+        </div>
+        <div id="Wednesday">
+          Wednesday.........................
+          {wednesday}
+        </div>
+        <div id="Thursday">
+          Thursday.........................
+          {thursday}
+        </div>
+        <div id="Friday">
+          Friday.........................
+          {friday}
+        </div>
+        <div id="Saturday">
+          Saturday.........................
+          {saturday}
+        </div>
+        <div id="Sunday">
+          Sunday.........................
+          {sunday}
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    const {
+    let {
       monday,
       tuesday,
       wednesday,
@@ -31,73 +75,27 @@ class OpenNow extends React.Component {
 
     const currentDay = this.props.currentDay;
 
-    // if (this.state.toggled === true) {
     return (
       <div className="wholeSchedule">
         <div className="oneDaySchedule" onClick={this.handleToggle.bind(this)}>
           <div>
-            <span className="openNowOneLine">Open Now</span>{' '}
-            {this.props.times[currentDay.toLowerCase()]}
-            <div className="chevron">
+            <span className="openNowOneLine inline">Open Now</span>{' '}
+            <span className="inline">
+              {this.props.times[currentDay.toLowerCase()]}
+            </span>
+            <span className="inline">
               <VelocityComponent
-                animation={{ rotateZ: this.state.expanded ? 180 : 0 }}
-                duration={500}
+                animation={{ rotateZ: this.state.expanded ? -180 : 0 }}
+                duration={200}
               >
                 <FaChevronDown />
               </VelocityComponent>
-            </div>
+            </span>
           </div>
         </div>
-        <div className="schedule" onClick={this.handleToggle.bind(this)}>
-          <div id="Monday">
-            Monday.........................
-            {monday}
-          </div>
-          <div id="Tuesday">
-            Tuesday.........................
-            {tuesday}
-          </div>
-          <div id="Wednesday">
-            Wednesday.........................
-            {wednesday}
-          </div>
-          <div id="Thursday">
-            Thursday.........................
-            {thursday}
-          </div>
-          <div id="Friday">
-            Friday.........................
-            {friday}
-          </div>
-          <div id="Saturday">
-            Saturday.........................
-            {saturday}
-          </div>
-          <div id="Sunday">
-            Sunday.........................
-            {sunday}
-          </div>
-        </div>
+        {this.state.toggled === true ? this.showSchedule() : null}
       </div>
     );
-    // } else {
-    //   return (
-    //     <div className="oneDaySchedule" onClick={this.handleToggle.bind(this)}>
-    //       <div>
-    //         <span className="openNowOneLine">Open Now</span>{' '}
-    //         {this.props.times[currentDay.toLowerCase()]}
-    //         <div className="chevronDown">
-    //           <VelocityComponent
-    //             animation={{ rotateZ: this.state.expanded ? 0 : -180 }}
-    //             duration={500}
-    //           >
-    //             <FaChevronDown />
-    //           </VelocityComponent>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   );
-    // }
   }
 }
 
