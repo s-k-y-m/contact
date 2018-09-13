94c2590497c0c93c30eb6c839a36d3a02a16ae4d
Samuel Shih <samuels.shih@gmail.com>
2018-09-13 12:37:03 -0700

    Fixed the chevron to display inline

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -12,10 +12,10 @@ const Map = () => {
         width="100%"
         height="600"
         src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-        frameborder="0"
+        frameBorder="0"
         scrolling="no"
-        marginheight="0"
-        marginwidth="0"
+        marginHeight="0"
+        marginWidth="0"
       >
         <a href="https://www.maps.ie/create-google-map/">Create Google Map</a>
       </iframe>
